7f6a27bb980796fba9567f06b850cf3c85b21a27
thanhloc <ngothanhloc.22102003@gmail.com>
2025-06-05 08:55:04 +0700

    update email

--- tests/utils/constants.js
+++ tests/utils/constants.js
@@ -1,11 +1,11 @@
 // utils/constants.js
-const BASE_URL = "https://mypod.io.vn/default-channel";
+const BASE_URL = "https://zoomprints.com/default-channel";
 const MAILINATOR_BASE_URL = "https://www.mailinator.com/v4/public/inboxes.jsp";
-const PRODUCTS_URL = "https://mypod.io.vn/default-channel/products";
-const CART_URL = "https://mypod.io.vn/default-channel/cart";
-const LOGIN_URL = "https://mypod.io.vn/default-channel/login";
-const ORDERS_URL = "https://mypod.io.vn/default-channel/orders";
-const SUPPORT_URL = "https://mypod.io.vn/default-channel/support";
+const PRODUCTS_URL = "https://zoomprints.com/default-channel/products";
+const CART_URL = "https://zoomprints.com/default-channel/cart";
+const LOGIN_URL = "https://zoomprints.com/default-channel/login";
+const ORDERS_URL = "https://zoomprints.com/default-channel/orders";
+const SUPPORT_URL = "https://zoomprints.com/default-channel/support";
 
 module.exports = {
   BASE_URL,
